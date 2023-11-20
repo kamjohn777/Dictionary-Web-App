a0d76ef7344b103260dc7822868ff802d3336f0d
Johnson <kameron.johnson@hp.com>
2023-11-20 16:55:50 -0600

    Updated the main.js with functions that update the text on broeser as well as css and html

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ function debounce(func, delay) {
     };
 }
 
+debounce();
+
 function fetchWordDefinition(word) {
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
@@ -20,44 +22,61 @@ function fetchWordDefinition(word) {
         }
         return res.json();
      })
-     .then((data) => console.log(data))
+     .then((data) => {
+        if (data.length > 0 && data[0].meanings) {
+            updateWordOnBrowser(word); 
+            updateDefinitionsOnBrowser(data[0].meanings);
+            updatePhoneticsOnBrowser(data[0].phonetics[0] && data[0].phonetics[0].text ? data[0].phonetics[0].text : '');
+        } else {
+            throw new Error('No meanings found for this word.')
+        }
+     })
     //  .then((data) => updateDefinitionsOnBrowser(data[0].meanings))
      .catch((error) => console.error('Fetch error:', error))
+     updateWordOnBrowser(''); // Clear the word
+            updatePhoneticsOnBrowser(''); // Clear the phonetics
+            updateDefinitionsOnBrowser([]); // Clear definitions
 }
 
-fetchWordDefinition("hey");
+fetchWordDefinition();
+
+function updatePhoneticsOnBrowser(phoneticText) {
+    const pronunciationElement = document.querySelector('.pronunciation');
+    pronunciationElement.textContent = phoneticText;
+}
+
+updatePhoneticsOnBrowser();
+
+// FUNCTION that updates the word on the browser
+function updateWordOnBrowser(word) {
+    const defineWordElement = document.querySelector('.word');
+    if (defineWordElement) { 
+        defineWordElement.textContent = word; 
+    }
+}
 
 
-// This function is going to take the API response and update the definitions of the web page based on the users input
-function updateDefinitionsOnBrowser(data) {
-    // const nounDefinitionList = document.querySelector('.parf-of-speech noun');
-    // const verbDefinitionList = document.querySelector('.part-of-speech verb');
-    const nounDefinitionList = document.querySelector('.part-of-speech.noun .definition ul');
-    const verbDefinitionList = document.querySelector('.part-of-speech.verb .definition ul');
+// FUNCTION that updates the definitions nouns and verbs
+function updateDefinitionsOnBrowser(meanings) {
+    // Clear previous definitions
+    const definitionContainers = document.querySelectorAll('.part-of-speech .definition ul');
+    definitionContainers.forEach(container => container.innerHTML = '');
 
+    meanings.forEach((meaning) => {
+        const partOfSpeech = meaning.partOfSpeech;
+        const definitions = meaning.definitions.map(def => `<li>${def.definition}</li>`).join('');
 
-    // this clears the previous definitions
-    nounDefinitionList.innerHTML = '';
-    verbDefinitionList.innerHTML = '';
-
-    // Logic for definitions
-    data.forEach((part) => {
-        if (part.partOfSpeech === 'noun') {
-            part.definitions.forEach((def) => {
-                const li = document.createElement('li');
-                li.textContent = def.definition;
-                nounDefinitionList.appendChild(li);
-            });
-        } else if (part.partOfSpeech === 'verb') {
-            part.definitions.forEach((def) => {
-                const li = document.createElement('li');
-                li.textContent = def.definition;
-                verbDefinitionList.appendChild(li);
-            });
+        // Find the container for the current part of speech
+        const definitionList = document.querySelector(`.part-of-speech.${partOfSpeech} .definition ul`);
+        if (definitionList) {
+            definitionList.innerHTML = definitions;
         }
     });
-} 
+}
 
+// we're passing an empty array as an argument because we're using the forEach method and this method can only be used for arrays before i tried passing a string and it gave me an error because forEach can only be used with arrays 
+updateDefinitionsOnBrowser([]);
+console.log(updateDefinitionsOnBrowser([]));
 
 
 // Event listener for the input field
