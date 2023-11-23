f5250f3b07c3c5d435834c70e73b724db7268de6
Johnson <kameron.johnson@hp.com>
2023-11-22 20:21:19 -0600

    updated and created the light adn dark theme of the web app

--- main.js
+++ main.js
@@ -48,6 +48,63 @@ function fetchWordDefinition(word) {
 
 fetchWordDefinition('Dictionary');
 
+// code for menu settings toggle
+// Function to toggle the theme
+function toggleTheme() {
+    const bodyElement = document.body;
+    const containerElement = document.querySelector('.container');
+    const wordElement = document.querySelector('.word');
+    const pronunciationElement = document.querySelector('.pronunciation');
+
+  // Toggle classes for body
+  bodyElement.classList.toggle('dark-mode');
+  bodyElement.classList.toggle('light-mode');
+
+  // Also toggle classes for the container, word, and pronunciation elements
+  containerElement.classList.toggle('dark-mode');
+  containerElement.classList.toggle('light-mode');
+  wordElement.classList.toggle('dark-mode');
+  wordElement.classList.toggle('light-mode');
+  pronunciationElement.classList.toggle('dark-mode');
+  pronunciationElement.classList.toggle('light-mode');
+}
+
+// Initial theme setup
+document.body.classList.add('light-mode'); // Set the initial theme to light
+// Set the initial theme for the container, word, and pronunciation as well
+const containerElement = document.querySelector('.container');
+const wordElement = document.querySelector('.word');
+const pronunciationElement = document.querySelector('.pronunciation');
+
+if (containerElement) {
+  containerElement.classList.add('light-mode');
+}
+if (wordElement) {
+  wordElement.classList.add('light-mode');
+}
+if (pronunciationElement) {
+  pronunciationElement.classList.add('light-mode');
+}
+
+
+// Event listener for the theme toggle button
+document.getElementById('toggleThemeButton').addEventListener('click', toggleTheme);
+
+// Initial theme setup
+document.body.classList.add('light-mode'); // Set the initial theme to light
+
+// Function to change the font
+function changeFont(font) {
+    document.body.style.fontFamily = font;
+}
+
+// Event listener for the font select dropdown
+document.getElementById('fontSelect').addEventListener('change', function() {
+    changeFont(this.value);
+});
+
+// end of code for menu settings
+
 function updatePhoneticsOnBrowser(phoneticText) {
     const pronunciationElement = document.querySelector('.pronunciation');
     pronunciationElement.textContent = phoneticText;
@@ -89,12 +146,25 @@ console.log(updateDefinitionsOnBrowser([]));
 // New function to update synonyms
 function updateSynonymsOnBrowser(synonyms) {
     const synonymsElement = document.querySelector('.synonyms');
-    const synonymsList = synonyms.map(synonym => `<a href="#" onclick="fetchWordDefinition('${synonym}'); return false;">${synonym}</a>`).join(', ');
-
-    // If there are synonyms, create a list; if not, display a message or leave it empty
-    synonymsElement.innerHTML = synonymsList.length > 0 ? synonymsList : 'No synonyms available.';
+    
+    // Check if the synonyms container exists
+    if (synonymsElement) {
+        let content;
+        if (synonyms && synonyms.length > 0) {
+            // Create links for each synonym
+            const synonymsLinks = synonyms.map(synonym => 
+                `<a href="#" onclick="fetchWordDefinition('${synonym}'); return false;">${synonym}</a>`
+            ).join(', ');
+            // Set the content to "Synonyms: " followed by the links
+            content = `Synonyms: ${synonymsLinks}`;
+        } else {
+            // If there are no synonyms, display a message
+            content = 'No synonyms available.';
+        }
+        // Update the innerHTML of the synonyms container
+        synonymsElement.innerHTML = content;
+    }
 }
-
 
 // Event listener for the input field
 document.getElementById('searchBar').addEventListener('input', debounce(function() {
